c5a470fc6035b0c35832f634448bb8c9792eb83f
a-funk <alexfunk1222@gmail.com>
2020-05-27 14:54:55 -0700

    Updates

---
+++ precache-manifest.62cc0e870f12dc9a8f591e607f6daada.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "440bc6583e7d791477d3426c66433c99",
+    "url": "/hcireact/index.html"
+  },
+  {
+    "revision": "0c6c0bf356b5aa7d8d1e",
+    "url": "/hcireact/static/css/2.5c17b9f1.chunk.css"
+  },
+  {
+    "revision": "770b4024096ad9012860",
+    "url": "/hcireact/static/css/main.947abab3.chunk.css"
+  },
+  {
+    "revision": "0c6c0bf356b5aa7d8d1e",
+    "url": "/hcireact/static/js/2.d3cfb087.chunk.js"
+  },
+  {
+    "revision": "2ca78df4e200ab631a9acbb7a85ee5b8",
+    "url": "/hcireact/static/js/2.d3cfb087.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "770b4024096ad9012860",
+    "url": "/hcireact/static/js/main.f6c31cd3.chunk.js"
+  },
+  {
+    "revision": "a89f75671470598e6d88",
+    "url": "/hcireact/static/js/runtime-main.2da39217.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/hcireact/static/media/logo.5d5d9eef.svg"
+  }
+]);
